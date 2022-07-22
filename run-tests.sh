aede209e52c02fa20d0ce78c43e0ea2179d26c21
Raphael Mitsch <r.mitsch@outlook.com>
2022-07-22 14:07:33 +0200

    Fix spaCy installation based on project.yml.

--- run-tests.sh
+++ run-tests.sh
@@ -65,9 +65,8 @@ do
       spacy_version=$(
         $python_interpreter -c "import srsly; data = srsly.read_yaml('${full_second_level_dir}/project.yml'); print(data.get('spacy_version', ''))"
       )
-
       if [ ! -z "$spacy_version" ]; then
-        $python_interpreter -m pip -q install "spacy${spacy_version}" --no-warn-script-location
+        $python_interpreter -m pip -I -q install "spacy${spacy_version}" --no-warn-script-location
       fi
 
       $python_interpreter -m pytest -q -s $full_second_level_dir
